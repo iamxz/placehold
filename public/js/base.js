797b405653b26a4121f433512cc064f8be0e4296
iamxz <xuzhoumail@gmail.com>
2014-07-11 23:36:05 +0800

    删除多余文件

--- public/js/base.js
+++ public/js/base.js
@@ -1,10 +1,10 @@
 $(function(){
     $("#create").click(function(){
-        var info = $("form").serialize();
+        var info = $("#form").serialize();
         var url = 'http://'+location.host+'/img?'+info;
         $("#url").val(url);
         $("#showImg").attr("src",url);
-
+        $("#pre").text('<img src="'+url+'">');
         $("#url").select();
     });
 });
